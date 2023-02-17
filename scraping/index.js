7b4e15b22dadc3ce4b766fec2cfbbdb658c772b3
cesar <caquino@dgrcorrientes.gov.ar>
2023-02-17 16:26:03 -0300

    conversion a number

--- scraping/index.js
+++ scraping/index.js
@@ -15,14 +15,14 @@ async function getLeaderBoard () {
     const $ = await scrape(URLS.leaderboard);
     const $rows = $('table tbody tr');
     
-    const LEADERBOARD_SELECTOR = {
-        team: '.fs-table-text_3',
-        victories: '.fs-table-text_4',
-        loses: '.fs-table-text_5',
-        scoreGoals: '.fs-table-text_6',
-        concededGoals: '.fs-table-text_7',
-        cardsYellow: '.fs-table-text_8',
-        cardsRed: '.fs-table-text_9'
+    const LEADERBOARD_SELECTORS = {
+        team: { selector: '.fs-table-text_3', typeOf: 'string' },
+        wins: { selector: '.fs-table-text_4', typeOf: 'number' },
+        loses: { selector: '.fs-table-text_5', typeOf: 'number' },
+        scoreGoals: { selector: '.fs-table-text_6', typeOf: 'number' },
+        concededGoals: { selector: '.fs-table-text_7', typeOf: 'number' },
+        yellowCards: { selector: '.fs-table-text_8', typeOf: 'number' },
+        redCards: { selector: '.fs-table-text_9', typeOf: 'number' }
     }
 
     const cleanText = (text) => text
@@ -30,50 +30,18 @@ async function getLeaderBoard () {
         .replace(/.*:/g, ' ')
         .trim()
     
-    const leaderBoardSelectorEntries = Object.entries(LEADERBOARD_SELECTOR);
+    const leaderBoardSelectorEntries = Object.entries(LEADERBOARD_SELECTORS);
 
-    $rows.each((index, el) => {
-        const leaderBoardEntries = leaderBoardSelectorEntries.map(([key, selector]) => {
+    $rows.each((_, el) => {
+        const leaderBoardEntries = leaderBoardSelectorEntries.map(([key, {selector, typeOf }]) => {
             const rawValue = $(el).find(selector).text();
-            const value = cleanText(rawValue);
+            const cleandValue = cleanText(rawValue);
+            const value = typeOf === 'number' 
+                ? Number(cleandValue) 
+                : cleandValue;
             return [ key, value ];
         });
         console.log(Object.fromEntries(leaderBoardEntries));
     });
-
-
-
-    
-        
-/*
-        const rawTeam = $el.find('.fs-table-text_3').text();
-        const rawVictories = $el.find('.fs-table-text_4').text();
-        const rawLoses = $el.find('.fs-table-text_5').text().trim();
-        const rawScoredGoals = $el.find('.fs-table-text_6').text();
-        const rawCondedeGoals = $el.find('.fs-table-text_7').text();
-        const rawCardsYellow = $el.find('.fs-table-text_8').text();
-        const rawCardsRed = $el.find('.fs-table-text_9').text();
-        console.log(
-            cleanText(rawTeam),
-            cleanText(rawVictories),
-            cleanText(rawLoses),
-            cleanText(rawScoredGoals),
-            cleanText(rawCondedeGoals),
-            cleanText(rawCardsYellow),
-            cleanText(rawCardsRed),
-        );*/
-    }
-
+}
 await getLeaderBoard()
-
-/*
-const leaderboard = [{
-    team: 'Team 2',
-    victories: 0,
-    loses: 0,
-    goalsScored: 0,
-    goalsConceded: 0,
-    cardsYellow: 0,
-    cardsRed: 0,
-}];
-*/
